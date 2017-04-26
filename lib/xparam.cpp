cc40d0cfe14b4736a0b8d90f2bfece040afb3c9f
merlokk <olegmsn@gmail.com>
2017-04-26 20:15:24 +0300

    add library xparam that saves parameters to EEPROM via json

--- lib/xparam.cpp
+++ lib/xparam.cpp
@@ -8,41 +8,63 @@ void xParam::begin(){
 
 }
 
-bool xParam::GetParamStr(String &paramName, String &paramValue)
-{
+bool xParam::GetParamStr(String &paramName, String &paramValue) {
 
+  return true;
 }
 
-bool xParam::LoadParamsFromEEPROM()
-{
+bool xParam::LoadParamsFromEEPROM() {
+  memset(&jsonMem[0], 0x00, JSON_MEM_BUFFER_LEN);
 
+  EEPROM.begin(JSON_MEM_BUFFER_LEN);
+//  EEPROM.get(0, jsonMem);
+  EEPROM.end();
+
+/*  if (jsonMem[JSON_MEM_BUFFER_LEN] != crc8(&jsonMem[0], JSON_MEM_BUFFER_LEN - 1)) {
+    memset(&jsonMem[0], 0x00, JSON_MEM_BUFFER_LEN);
+    DEBUG_PRINTLN(F("Invalid settings in EEPROM, settings was cleared."));
+  }
+*/
+  return true;
 }
 
-bool xParam::SaveParamsToEEPROM()
-{
+bool xParam::SaveParamsToEEPROM() {
+//  jsonMem[JSON_MEM_BUFFER_LEN] = crc8(&jsonMem[0], JSON_MEM_BUFFER_LEN - 1);
 
+  EEPROM.begin(JSON_MEM_BUFFER_LEN);
+
+  EEPROM.end();
+
+  return true;
 }
 
-bool xParam::LoadParamsFromWeb(String &url)
-{
+bool xParam::LoadParamsFromWeb(String &url) {
 
+  return true;
 }
 
-void xParam::GetParamsJsonStr(String &str)
-{
-  str = "";
-  JsonObject& root = jsonBuffer.createObject();
-  root.printTo(str);
+void xParam::GetParamsJsonStr(String &str) {
+  str = String((const char*)jsonMem);
 }
 
-template<typename T>
-bool xParam::SetParam(String &paramName, T &paramValue)
-{
-
+void xParam::SetLogger(xLogger *_logger) {
+  logger = _logger;
 }
 
-template<typename T>
-bool xParam::GetParam(String &paramName, T &param)
-{
-
+// non class functions
+uint8_t crc8(const uint8_t *data, uint8_t len) {
+  uint8_t crc = 0x00;
+  while (len--) {
+    uint8_t extract = *data++;
+    for (byte tempI = 8; tempI; tempI--) {
+      uint8_t sum = (crc ^ extract) & 0x01;
+      crc >>= 1;
+      if (sum) {
+        crc ^= 0x8C;
+      }
+      extract >>= 1;
+    }
+  }
+  return crc;
 }
+
--- lib/xparam.h
+++ lib/xparam.h
@@ -1,10 +1,15 @@
-#ifndef XPARAM_H
-#define XPARAM_H
+#ifndef __XPARAM_H
+#define __XPARAM_H
 
 #include <Arduino.h>
+#include <EEPROM.h>
 #include <ArduinoJson.h>
+#include <xlogger.h>
 
-#define JSON_MEM_BUFFER 512
+#define DEBUG
+
+#define JSON_OBJ_BUFFER_LEN 500
+#define JSON_MEM_BUFFER_LEN 512
 
 class xParam
 {
@@ -13,10 +18,12 @@ public:
 
   void begin();
 
-  template<typename T>
-  bool SetParam(String &paramName, T &paramValue);
-  template<typename T>
-  bool GetParam(String &paramName, T &param);
+  template<typename T, typename TString>
+  bool SetParam(const TString &paramName, const T &paramValue);
+  template<typename T, typename TString>
+  bool GetParam(const TString &paramName, T &param);
+  template<typename T, typename TString>
+  bool GetParamDef(const TString &paramName, T &param, String &defaultParamValue);
   bool GetParamStr(String &paramName, String &paramValue);
 
   bool LoadParamsFromEEPROM();
@@ -25,8 +32,74 @@ public:
 
   void GetParamsJsonStr(String &str);
 
+  void SetLogger(xLogger * _logger);
+  template <typename... Args>
+  void DEBUG_PRINTLN(Args... args);
+
 private:
-  StaticJsonBuffer<JSON_MEM_BUFFER> jsonBuffer;
+  xLogger* logger = NULL;
+  char jsonMem[JSON_MEM_BUFFER_LEN] = {0};
 };
 
-#endif // XPARAM_H
+// not in class
+
+uint8_t crc8(const uint8_t *data, uint8_t len);
+
+
+// templates
+
+template<typename T, typename TString>
+bool xParam::SetParam(const TString &paramName, const T& paramValue) {
+
+  StaticJsonBuffer<JSON_OBJ_BUFFER_LEN> jsonBuffer;
+  JsonObject *root = &jsonBuffer.parseObject(String(jsonMem));
+  if (!root->success()){
+    DEBUG_PRINTLN("SetParam: json load error.");
+    root = &jsonBuffer.createObject();
+  };
+  if (!root->success())
+    DEBUG_PRINTLN("SetParam: json final check load error.");
+
+  root->set(paramName, paramValue);
+
+  int n = root->printTo(&jsonMem[0], JSON_MEM_BUFFER_LEN);
+  jsonMem[n] = 0x00;
+  return root->success();
+}
+
+template<typename T, typename TString>
+bool xParam::GetParam(const TString &paramName, T &param) {
+
+  StaticJsonBuffer<JSON_OBJ_BUFFER_LEN> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(String(jsonMem));
+  if (!root.success() || !root.containsKey(paramName) ) {//|| !(root[paramName].is<T>()))
+    DEBUG_PRINTLN("GetParam: error loading json.");
+    return false;
+  }
+
+  param = root.get<T>(paramName);
+
+  return true;
+}
+
+template<typename T, typename TString>
+bool xParam::GetParamDef(const TString &paramName, T &param, String &defaultParamValue) {
+  if (!GetParam(paramName, param)) {
+    param = defaultParamValue;
+  }
+
+  return true;
+}
+
+template <typename... Args>
+void xParam::DEBUG_PRINTLN(Args... args) {
+#ifdef DEBUG
+  if (logger) {
+    logger->println(args...);
+  }
+#endif
+}
+
+
+
+#endif // __XPARAM_H
