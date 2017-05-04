7d5742801b3af19a485fd142edaa10b27cffea1b
merlokk <olegmsn@gmail.com>
2017-05-04 20:07:38 +0300

    esp14 sketch competed. cpu not returnning from deepsleep

--- ESP14MES/ESP14MES.ino
+++ ESP14MES/ESP14MES.ino
@@ -18,18 +18,20 @@
 // my libraries
 #include <etools.h>
 #include <pitimer.h>     // timers
-#include <eastron.h>
+#include <modbuspoll.h>
 #include <xparam.h>
 #include "general.h"
 
-#define               PROGRAM_VERSION   "0.5"
+#define               PROGRAM_VERSION      "0.5"
 
 #define               DEBUG                            // enable debugging
-#define               DEBUG_SERIAL      logger
+#define               DEBUG_SERIAL         logger
 
 // device modbus address
-#define               MODBUS_ADDRESS    42
-#define               MODBUS_OBJ_NAME   esp14
+#define               MODBUS_ADDRESS       42
+#define               MODBUS_OBJ_NAME      esp14
+
+#define               DEEPSLEEP_INTERVAL   10*1000000  // 60 sec
 
 // LEDs and pins
 #define PIN_PGM  0      // programming pin
@@ -38,8 +40,93 @@
 #define LEDON    LOW
 #define LEDOFF   HIGH
 
+#define DI_PD2         0x04 
+#define DI_PD3         0x08
+#define DI_WIFISETUP   DI_PD2
+#define DI_NODEEPSLEEP DI_PD3
+
 // objects
 ModbusPoll       esp14;
+
+struct Esp14Var {
+  int vLight;
+  int vPressure;
+  float vPrTemp;
+  float vHumidity;
+  float vTemp;
+  uint16_t vDI;
+} var {};
+
+void PollAndPublish(bool withInit = false) {
+  // modbus poll
+  esp14.SetDeviceAddress(MODBUS_ADDRESS);
+  esp14.SetLogger(&logger);
+  DEBUG_PRINT(F("DeviceType: "));
+  DEBUG_PRINTLN(params[F("device_type")]);
+  esp14.ModbusSetup(params[F("device_type")].c_str());
+
+  String str;
+  esp14.getStrModbusConfig(str);
+  DEBUG_PRINTLN(F("Modbus config:"));
+  DEBUG_PRINTLN(str);
+
+  // poll
+  esp14.Poll(POLL_INPUT_REGISTERS);
+  if (!esp14.Connected) {
+    DEBUG_EPRINTLN(F("Modbus device is not connected."));
+    return;
+  }
+
+  // get values and normalize
+  var.vLight    = esp14.getWordValue(POLL_INPUT_REGISTERS, 0x00);
+  var.vPressure = esp14.getWordValue(POLL_INPUT_REGISTERS, 0x01) + 60000;
+  var.vPrTemp   = esp14.getWordValue(POLL_INPUT_REGISTERS, 0x02) / 100;
+  var.vHumidity = esp14.getWordValue(POLL_INPUT_REGISTERS, 0x03) / 10;
+  var.vTemp     = esp14.getWordValue(POLL_INPUT_REGISTERS, 0x04) / 10;
+  var.vDI       = esp14.getWordValue(POLL_INPUT_REGISTERS, 0x05);
+
+
+  // wait wifi to connect   
+  int cnt = 0;
+  while (WiFi.status() != WL_CONNECTED && cnt < 60) { // 6 sec for connect
+    delay(100);
+    cnt ++;
+  }
+  if (WiFi.status() != WL_CONNECTED) {
+    DEBUG_WPRINTLN(F("Wifi is not connected."));
+    return;
+  } else {
+    DEBUG_EPRINTLN(SF("Wifi is connected. Timeout ms=") + String(cnt * 100));
+  }
+
+  // mqtt init and pub
+  if (withInit) {
+    initMQTT("esp14");
+  }
+
+  // publish some system vars
+  if (withInit) {
+    mqttPublishInitialState();
+  }
+  mqttPublishRegularState();
+
+  // publish vars from configuration
+  if (esp14.Connected) {
+    String str;
+    str = String(var.vLight);
+    mqttPublishState("Light", str.c_str());        
+    str = String(var.vPressure);
+    mqttPublishState("Pressure", str.c_str());        
+    str = String(var.vPrTemp);
+    mqttPublishState("PrTemp", str.c_str());        
+    str = String(var.vHumidity);
+    mqttPublishState("Humidity", str.c_str());        
+    str = String(var.vTemp);
+    mqttPublishState("Temp", str.c_str());        
+    str = String(var.vDI, HEX);
+    mqttPublishState("DI", str.c_str());        
+  };
+}
 
 void setup() {  
   Serial.setDebugOutput(false);
@@ -66,35 +153,26 @@ void setup() {
   // connect to default wifi
   WiFi.begin();
 
-  // modbus poll
-  esp14.SetDeviceAddress(MODBUS_ADDRESS);
-  esp14.SetLogger(&logger);
-  DEBUG_PRINT(F("DeviceType: "));
-  DEBUG_PRINTLN(params[F("device_type")]);
-  esp14.ModbusSetup(params[F("device_type")].c_str());
-
-  String str;
-  esp14.getStrModbusConfig(str);
-  DEBUG_PRINTLN(F("Modbus config:"));
-  DEBUG_PRINTLN(str);
-
-  // wait wifi to connect   
-  if (WiFi.status() != WL_CONNECTED) {
-    DEBUG_WPRINTLN(F("Wifi is not connected. Wait..."));
-    delay(5000);
+  PollAndPublish(true);
+  
+  if ((var.vDI & DI_NODEEPSLEEP) != 0) {
+    DEBUG_PRINTLN(F("Go to deep sleep..."));
+    ESP.deepSleep(DEEPSLEEP_INTERVAL);
+    delay(1000);
   }
 
-  // mqtt init and pub
-  initMQTT("esp14");
-
-
-
-  ESP.deepSleep(60 * 1000000); // 60 sec
-
   // config here
+  
+  // ArduinoOTA
+  setupArduinoOTA();
 }
 
 void loop() {
+  ArduinoOTA.handle();
+  logger.handle();
 
+  yield();
+
+  PollAndPublish();
 
 }
