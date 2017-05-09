7ebf3f8189c3a03f6d718ee2083a187087b22cfb
Oleg Moiseenko <olegmsn@gmail.com>
2017-05-09 23:49:10 +0300

    lib xmqtt add some logic

--- lib/xmqtt.cpp
+++ lib/xmqtt.cpp
@@ -4,7 +4,152 @@ xMQTT::xMQTT() {
 
 }
 
+void xMQTT::begin(const char *_topicName, xParam *_params, xLogger *_logger, bool _postAsJson, bool retained) {
+  SetTopicName(_topicName);
+  SetParamStorage(_params);
+  SetLogger(_logger);
+  SetPostAsJson(_postAsJson);
+}
+
+void xMQTT::SetParamStorage(xParam *_params) {
+  params = _params;
+}
+
 void xMQTT::SetLogger(xLogger * _logger) {
   logger = _logger;
 }
 
+void xMQTT::SetPostAsJson(bool _postAsJson) {
+  postAsJson = _postAsJson;
+}
+
+void xMQTT::SetTopicName(const char *defaultTopicName) {
+  mqttClient.setCallback(mqttCallback);
+
+  // configure mqtt topic
+  String mqttPath = params[F("mqtt_path")];
+  if (mqttPath.length() == 0) {
+    mqttTopic = hardwareID + "/" + defaultTopicName + "/";
+  } else {
+    if (mqttPath[mqttPath.length() - 1] == '/') {
+      mqttTopic = mqttPath;
+    } else {
+      mqttTopic = mqttPath + "/";
+    }
+  }
+  DEBUG_PRINTLN(SF("MQTT command topic: ") + mqttTopic);
+}
+
+void xMQTT::SetDefaultRetained(bool _retained) {
+  retained = _retained;
+}
+
+void xMQTT::SetCmdCallback(cmdCallback) {
+  _cmdCallback = cmdCallback;
+}
+
+void xMQTT::execCmdCallback(String &cmd) {
+  if (_cmdCallback) {
+    _cmdCallback(cmd);
+  }
+}
+
+void xMQTT::mqttCallback(char* topic, byte* payload, unsigned int length) {
+  String sPayload;
+  BufferToString(sPayload, (char*)payload, length);
+
+  DEBUG_PRINTLN(SF("MQTT message arrived [") + topic + SF("] ") + sPayload);
+
+  // commands
+  execCmdCallback(sPayload);
+}
+
+
+bool xMQTT::Connect() {
+  if (mqttClient.connected()) {
+    return true;
+  }
+
+  uint8_t i = 0;
+  String srv = params[F("mqtt_server")];                                                                      // extend visiblity of the "mqtt_server" parameter
+  mqttClient.setServer(srv.c_str(), params[F("mqtt_port")].toInt());
+  while (!mqttClient.connected()) {
+    if (mqttClient.connect(hardwareID.c_str(), params[F("mqtt_user")].c_str(), params[F("mqtt_passwd")].c_str())) {  // because connect doing here
+      DEBUG_PRINTLN(F("The client is successfully connected to the MQTT broker"));
+
+      // subscribe to control topic
+      String vtopic = mqttTopic + SF("control");
+      mqttClient.subscribe(vtopic.c_str());
+      resetErrorCnt = 0;
+    } else {
+      DEBUG_PRINTLN(llError, SF("The connection to the MQTT broker failed. User: ") + params[F("mqtt_user")] +
+        SF(" Passwd: ") + params[F("mqtt_passwd")] +  SF(" Broker: ") + srv + SF(":") + params[F("mqtt_port")]);
+
+      delay(1000);
+
+      if (i >= 2) {
+        break;
+      }
+      i++;
+
+      if (resetErrorCnt >= 50) {
+        restart();
+      }
+      resetErrorCnt++;
+    }
+  }
+}
+
+bool xMQTT::Disconnect() {
+  if (!mqttClient.connected()) {
+    return true;
+  }
+  mqttClient.disconnect();
+}
+
+bool xMQTT::Reconnect() {
+  if (Disconnect())
+    return Connect();
+  return false;
+}
+
+void xMQTT::BeginPublish() {
+  if (!postAsJson)
+    return;
+
+  jsonBuffer.reserve(BUF_RESERVE_LEN);
+  jsonBuffer = "{";
+}
+
+void xMQTT::PublishState(const char *topic, const char *payload) {
+  PublishState(String(topic), String(payload))
+}
+
+void xMQTT::PublishState(String &topic, String &payload) {
+  if (postAsJson) {
+    jsonBuffer += "\"" + topic + "\":\"" + payload + "\",";
+  } else {
+    String vtopic = mqttTopic + topic;
+    if (mqttClient.publish(vtopic.c_str(), payload.c_str(), retained)) {
+      DEBUG_PRINTLN(SF("MQTT publish ok. Topic: ") + vtopic + SF(" Payload: ") + payload);
+    } else {
+      DEBUG_PRINTLN(llError, F("MQTT message publish failed"));
+    }
+  }
+
+}
+
+void xMQTT::Commit(String &topicAdd) {
+  if (!postAsJson)
+    return;
+  if (jsonBuffer[jsonBuffer.length() - 1] = ',')
+    jsonBuffer.remove(jsonBuffer.length() - 1);
+  jsonBuffer += "}";
+
+  String vtopic = mqttTopic + topicAdd;
+  if (mqttClient.publish(vtopic.c_str(), jsonBuffer.c_str(), retained)) {
+    DEBUG_PRINTLN(SF("MQTT publish ok. Topic: ") + vtopic + SF(" Payload: ") + jsonBuffer);
+  } else {
+    DEBUG_PRINTLN(llError, F("MQTT message publish failed"));
+  }
+}
--- lib/xmqtt.h
+++ lib/xmqtt.h
@@ -3,17 +3,26 @@
 
 #include <Arduino.h>
 #include <etools.h>
+#include <xparam.h>
 #include <xlogger.h>        // logger https://github.com/merlokk/xlogger
+
+#define MQTT_DEBUG
+#define BUF_RESERVE_LEN 256 // reserve for json string
+
+typedef void (*cmdCallback)(String &cmd);
 
 class xMQTT {
 public:
   xMQTT();
 
-  void begin(const char * topicName, xLogger * _logger = NULL, bool _postAsJson = false, bool retained = false);
-  void init(const char * topicName);
-  void SetLogger(xLogger * _logger);
+  void begin(const char *_topicName, xParam *_params, xLogger *_logger = NULL, bool _postAsJson = false, bool retained = false);
+  void SetParamStorage(xParam *_params);
+  void SetLogger(xLogger *_logger);
   void SetPostAsJson(bool _postAsJson);
   void SetTopicName(const char * topicName);
+  void SetDefaultRetained(bool _retained);
+
+  void SetCmdCallback(cmdCallback);
 
   bool Connect();
   bool Disconnect();
@@ -25,14 +34,32 @@ public:
   void Commit(String &topicAdd);           // works for json push
 
 private:
+  xParam * params = NULL;
   xLogger * logger = NULL;
   bool postAsJson = false;
   bool retained = false;
+  int resetErrorCnt = 0;  // connect error counter
+  String jsonBuffer = "";
+
+  String mqttTopic;
+  String hardwareID;
+
+  cmdCallback _cmdCallback;
+  void execCmdCallback(String &cmd);
 
   void mqttCallback(char* topic, byte* payload, unsigned int length);
 
   template <typename... Args>
   void DEBUG_PRINTLN(Args... args);
 };
+
+template<typename... Args>
+void xMQTT::DEBUG_PRINTLN(Args... args) {
+#ifdef MQTT_DEBUG
+  if (logger) {
+    logger->println(args...);
+  }
+#endif
+}
 
 #endif // XMQTT_H
