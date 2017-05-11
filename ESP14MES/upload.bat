678365fcbcfce6bdb5497c68fd91855a2873aeac
merlokk <olegmsn@gmail.com>
2017-05-11 19:54:22 +0300

    upload command file

---
+++ ESP14MES/upload.bat
@@ -0,0 +1,4 @@
+C:\Users\oleg\AppData\Local\Arduino15\packages\esp8266\tools\esptool\0.4.9/esptool.exe -vvv -cb 115200 -cp COM17 -bz 1M -ce
+pause
+C:\Users\oleg\AppData\Local\Arduino15\packages\esp8266\tools\esptool\0.4.9/esptool.exe -vv -cd ck -cb 921600 -cp COM17 -ca 0x00000 -cf C:\Users\oleg\AppData\Local\Temp\arduino_build_585678/ESP14MES.ino.bin
+pause
