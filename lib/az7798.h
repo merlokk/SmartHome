3936af29c82cea64d155e158b13079c647376d4f
merlokk <olegmsn@gmail.com>
2017-05-12 21:46:14 +0300

    some sketch for AZ7798

--- lib/az7798.h
+++ lib/az7798.h
@@ -1,10 +1,48 @@
 #ifndef AZ7798_H
 #define AZ7798_H
 
+#define AZ_DEBUG
+
+enum AZState {
+  asInit         = 0x00,
+  asWait         = 0x01,
+  asSentCommand  = 0x02,
+  asGotResponse  = 0x03,
+  asTimeout      = 0x04
+};
+
+enum AZProcessCommands {
+  acNone             = 0x00,
+  acGetVersion       = 0x01,
+  acGetDateTime      = 0x02,
+  acSetDateTime      = 0x03,
+  acGetMeasurements  = 0x04
+};
 
 class az7798 {
 public:
   az7798();
+
+  void begin();
+  void handle();
+
+
+
+  // strings from AZ
+  String Version;
+  String DateTime;
+  bool SetDateTime;
+  String Measurements;
+  // decoded measurements
+  float Temperature;
+  float Humidity;
+  int CO2;
+
+  //last connect to AZ
+  int LastGetMeasurements;
+
+
+private:
 };
 
 #endif // AZ7798_H
