2afdf4bca8b7af0e1f3338dd921054a3297d59bd
merlokk <olegmsn@gmail.com>
2018-06-15 13:32:12 +0300

    added upload.bat for uploading firmware sample

--- ESP8266EASTRON/upload.bat
+++
@@ -1,1 +0,0 @@
-python.exe C:\Users\yadro\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.3.0/tools/espota.py -i 192.168.1.10 -p 8266 --auth=123454321 -f C:\Users\yadro\AppData\Local\Temp\arduino_build_901765/ESP8266EASTRON.ino.bin
---
+++ ESP8266EASTRON/upload.cmd
@@ -0,0 +1,1 @@
+python.exe C:\Users\oleg\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.3.0/tools/espota.py -i 192.168.55.10 -p 8266 --auth=123454321 -f C:\Users\oleg\AppData\Local\Temp\arduino_build_673477/ESP8266EASTRON.ino.bin
