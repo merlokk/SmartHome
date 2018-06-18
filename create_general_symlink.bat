f723bee9ed2813410ef923f0b01514c63d72014a
merlokk <olegmsn@gmail.com>
2018-06-18 17:09:33 +0300

    added some symlinks creation

--- create_general_symlink.bat
+++ create_general_symlink.bat
@@ -5,5 +5,7 @@ mklink  %drivep%\ESP14MES\general.h %drivep%\ESP8266EASTRON\general.h
 mklink  %drivep%\ESP14MES\general.ino %drivep%\ESP8266EASTRON\general.ino
 mklink  %drivep%\ESP8266AZ7798\general.h %drivep%\ESP8266EASTRON\general.h
 mklink  %drivep%\ESP8266AZ7798\general.ino %drivep%\ESP8266EASTRON\general.ino
+mklink  %drivep%\ESP8266CO2PM25\general.h %drivep%\ESP8266EASTRON\general.h
+mklink  %drivep%\ESP8266CO2PM25\general.ino %drivep%\ESP8266EASTRON\general.ino
 pause
 
